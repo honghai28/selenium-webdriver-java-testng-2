6e84df63469eab23031fc30cf39d11b19368cc79
honghai28 <haihong419@gmail.com>
2025-02-09 18:49:52 +0700

    Add Topic_13

---
+++ selenium/javaTester/Topic_01_Data_type.java
@@ -0,0 +1,32 @@
+package javaTester;
+
+import org.openqa.selenium.JavascriptException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxCommandContext;
+
+public class Topic_01_Data_type {
+    //Kiểu dữ liệu trong java- 2 nhóm
+    //I. kiểu dữ liệu nguyên thủy
+    //8 loại
+    //Số nguyên:byte-sort-int-long
+    //Số thực:float-double
+    // ký tự: char
+    //logic:boolean
+    //II. Kiểu d liệu tham chiếu
+    //class
+
+    //interface
+    WebDriver webriver;
+    JavascriptException javascriptException;
+    //object
+    Object name = "";
+    //array
+    int[] abc ={};
+
+//khai báo 1 biến để lưu trữ 1 loại dữ liệu nào đó
+    //Access Modifier: Phạm vi truy cập (public,private/protected/default)
+    //kiểu dữ liệu
+    //tên biến
+    // giá trị của biến - gán bang phép =
+
+}
